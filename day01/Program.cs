879fb98f274089aaacd10c89ba4b198c939d2d0f
Thomas Rayner <thmsrynr@outlook.com>
2021-12-01 08:05:30 -0800

    day 01 part 01

---
+++ day01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // pt 1
+            var input = File.ReadAllLines(args[0]).Select(int.Parse).ToArray();
+            var pt1 = 0;
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] > input[i - 1]) pt1++;
+            }
+
+            Console.WriteLine($"Part 1: {pt1}");
+        }
+    }
+}
